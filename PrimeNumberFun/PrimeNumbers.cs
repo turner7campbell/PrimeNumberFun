16924a5043601bbbbb8d0f4cb091113fe56741f3
nicomp42 <nicholdw@ucmail.uc.edu>
2024-03-31 10:18:26 -0400

    Add project files.

---
+++ PrimeNumberFun/PrimeNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberFun {
+    internal class PrimeNumbers {
+        public static Boolean IsPrime(long num) {
+            if (num == 2)     { return true;  }
+            if (num == 3)     { return true;  }
+            if (num % 2 == 0) { return false; }
+
+            for (int i = 3; i <= Math.Sqrt(num); i += 2) {
+                if (num % i == 0) { return false; }
+            }
+            return true;    
+        }
+    }
+}
---
+++ PrimeNumberFun/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Prime Number Fun
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberFun {
+    internal class Program {
+        static void Main(string[] args)
+        {
+            int passCount = 0, failCount = 0;
+            long[] test = new long[]           {   2,     3,     4,    5,     6,    7,     8,     9,    10,   11 };
+            Boolean[] expected = new Boolean[] { true, true, false, true, false, true, false, false, false, true };
+            for (int i = 0; i < test.Length; i++) {
+                Console.Write(test[i] + ": " + PrimeNumberFun.PrimeNumbers.IsPrime(test[i]));
+                if (PrimeNumbers.IsPrime(test[i]) != expected[i])
+                {
+                    Console.WriteLine("FAILED"); 
+                    failCount++;
+                } else { Console.WriteLine(""); passCount++; }
+            }
+            Console.WriteLine("Passed: " + passCount);
+            Console.WriteLine("Failed: " + failCount);
+        }
+    }
+}
